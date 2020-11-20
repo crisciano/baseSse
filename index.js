58e71dd2efd0875e15916fdbf850546f9c591ee3
Crisciano da Silva Botelho <crisciano.botelho@pampa.compasso>
2020-11-20 16:42:16 -0300

    Include dotenv

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = require('./src/app')
 const { logger } = require('./src/utils/logger')
 const returnMessage = require('./src/utils/Message')
 const env = require('./src/utils/EnvironmentVariables')
+const dotenv = require('dotenv');
+dotenv.config();
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 const port = env.port()
